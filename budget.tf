ec2982c8742cc7bc294f8a3cc07ae9ba5ffcaced
Naoki Oketani <okepy.naoki@gmail.com>
2020-05-03 14:57:10 +0900

    create budget to notify me if actual cost > $30

---
+++ budget.tf
@@ -0,0 +1,16 @@
+resource "aws_budgets_budget" "cost" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "30"
+  limit_unit        = "USD"
+  time_period_start = "2020-05-01_00:00"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["okepy.naoki@gmail.com"]
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+  }
+}
