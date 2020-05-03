e1ba5488bf305f1b994f7372a3b4253b8ddc4029
Naoki Oketani <okepy.naoki@gmail.com>
2020-05-03 14:46:22 +0900

    initial setup

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.12.0"
+  backend "s3" {
+    bucket = "tfstate-ng46"
+    region = "us-east-1"
+    key = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tfstate-ng46"
+  versioning {
+    enabled = true
+  }
+}
