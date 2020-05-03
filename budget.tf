6efd66d443c5bda9c0378316dba48a4cfbd26b5f
Naoki Oketani <okepy.naoki@gmail.com>
2020-05-03 15:38:44 +0900

    fix cost limit_amount

--- budget.tf
+++ budget.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "cost" {
   name              = "monthly-budget"
   budget_type       = "COST"
-  limit_amount      = "30"
+  limit_amount      = "30.0"
   limit_unit        = "USD"
   time_period_start = "2020-05-01_00:00"
   time_unit         = "MONTHLY"
