8658bc7523531e82830b1628c679bcfbb9dbfec3
Naoki Oketani <okepy.naoki@gmail.com>
2020-05-17 09:04:20 +0900

    set Config Rules

--- modules/aws/config/main.tf
+++ modules/aws/config/main.tf
@@ -22,3 +22,45 @@ resource "aws_config_configuration_recorder_status" "config-rec-status" {
   is_enabled = true
   depends_on = [aws_config_delivery_channel.config-dc]
 }
+
+########################################
+# Config Rules
+########################################
+
+resource "aws_config_config_rule" "access-keys-rotated" {
+  name = "access-keys-rotated"
+  input_parameters = <<EOF
+{
+  "maxAccessKeyAge": "180"
+}
+EOF
+
+  source {
+    owner             = "AWS"
+    source_identifier = "ACCESS_KEYS_ROTATED"
+  }
+
+  depends_on = [aws_config_configuration_recorder.config-rec]
+}
+
+resource "aws_config_config_rule" "s3-bucket-public-read-prohibited" {
+  name = "s3-bucket-public-read-prohibited"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_READ_PROHIBITED"
+  }
+
+  depends_on = [aws_config_configuration_recorder.config-rec]
+}
+
+resource "aws_config_config_rule" "s3-bucket-public-write-prohibited" {
+  name = "s3-bucket-public-write-prohibited"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_PUBLIC_WRITE_PROHIBITED"
+  }
+
+  depends_on = [aws_config_configuration_recorder.config-rec]
+}
