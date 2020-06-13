50e0b2b400cbc577faaf78161309eead43956fa8
Naoki Oketani <okepy.naoki@gmail.com>
2020-06-13 12:15:43 +0900

    create SNS topic for Config (#3)
    * create SNS topic for Config
    
    * add SNS topic relation to config delivery channel
    
    * add new line at EOF
    
    * fix invalid workflow file
    
    * rename job id
    
    * remove actions/github-script step
    
    * configure AWS credentials
    
    * fix invalid syntax
    
    * rename job
    
    * create fmt, validate jobs
    
    * a step cannot have both the `uses` and `run` keys
    
    * fix Error: No valid credential sources found for AWS Provider.

--- modules/aws/config/main.tf
+++ modules/aws/config/main.tf
@@ -6,6 +6,7 @@ data "aws_caller_identity" "self" {}
 resource "aws_config_delivery_channel" "config-dc" {
   name           = "aws-config-${var.region}"
   s3_bucket_name = var.bucket
+  sns_topic_arn  = aws_sns_topic.config_alert.arn
   depends_on     = [aws_config_configuration_recorder.config-rec]
 }
 
@@ -63,4 +64,12 @@ resource "aws_config_config_rule" "s3-bucket-public-write-prohibited" {
   }
 
   depends_on = [aws_config_configuration_recorder.config-rec]
+}
+
+########################################
+# SNS
+########################################
+
+resource "aws_sns_topic" "config_alert" {
+  name = "config-alert"
 }
