5e8c279b1f5742c907c1ee56fd613b6b0721f53d
Naoki Oketani <okepy.naoki@gmail.com>
2020-07-24 21:02:34 +0900

    quote type name

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_organizations_organization" "org" {
     "config.amazonaws.com",
   ]
   enabled_policy_types = [
-    SERVICE_CONTROL_POLICY
+    "SERVICE_CONTROL_POLICY"
   ]
   feature_set = "ALL"
 }
