526758f95024b1daf0603028e419f530fd9d808b
Naoki Oketani <okepy.naoki@gmail.com>
2021-01-26 22:05:33 +0900

    fix: auto enable guardduty for organization (#19)
    https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/guardduty_organization_configuration

--- modules/aws/guardduty/main.tf
+++ modules/aws/guardduty/main.tf
@@ -4,6 +4,11 @@ resource "aws_guardduty_detector" "guardduty" {
   enable = true
 }
 
+resource "aws_guardduty_organization_configuration" "org-gd" {
+  auto_enable = true
+  detector_id = aws_guardduty_detector.guardduty.id
+}
+
 resource "aws_cloudformation_stack" "amazon-guardduty-to-slack" {
   name = "amazon-guardduty-to-slack"
   capabilities = ["CAPABILITY_IAM"]
