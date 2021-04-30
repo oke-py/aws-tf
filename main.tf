3ecc3bb9f7bf03f5e0ae9a10147a21e622729d9d
Naoki Oketani <okepy.naoki@gmail.com>
2021-04-30 22:28:26 +0900

    cleanup: specify required_providers

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
   backend "s3" {
     bucket  = "tfstate-ng46"
     region  = "us-east-1"
