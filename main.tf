91fd6676e06b559d8310cf9c0534e03d62ab949b
Naoki Oketani <okepy.naoki@gmail.com>
2021-10-04 21:43:32 +0900

    GitHub Actions use OIDC provider (#42)
    * GitHub Actions use OIDC provider
    
    * add permissions for github action jobs
    
    * run terraform fmt
    
    * add permission to validate and plan jobs
    
    * debug: make sure AssumeRole succeed
    
    * remove debug step

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
--- variables.tf
+++ variables.tf
@@ -1,5 +1,3 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
 variable "org_admin_id" {}
 variable "org_account1_id" {}
 variable "slack_aws_alert_url" {}
