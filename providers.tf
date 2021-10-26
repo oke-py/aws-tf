8582788e6f75797cbb1043881e5fa9127b0f98a1
Naoki Oketani <okepy.naoki@gmail.com>
2021-10-26 20:58:24 +0900

    set default tags (#46)
    * set default tags
    
    * fix an error

--- providers.tf
+++ providers.tf
@@ -1,9 +1,21 @@
 provider "aws" {
   region = "us-east-1"
   alias  = "Virginia"
+  default_tags {
+    tags = {
+      managed-by = "terraform"
+      repo       = "github.com/oke-py/aws-tf"
+    }
+  }
 }
 
 provider "aws" {
   region = "ap-northeast-1"
   alias  = "Tokyo"
+  default_tags {
+    tags = {
+      managed-by = "terraform"
+      repo       = "github.com/oke-py/aws-tf"
+    }
+  }
 }
