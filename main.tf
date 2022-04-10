daf8965d0666ec19e1e4725cdcc213c682e66691
Naoki Oketani <okepy.naoki@gmail.com>
2022-04-10 14:57:15 +0900

    migrate to Terraform Cloud

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
-  backend "s3" {
-    bucket  = "outside-code-management"
-    region  = "ap-northeast-1"
-    key     = "infra.state"
-    encrypt = true
+  cloud {
+    organization = "oke-py"
+    workspaces {
+      name = "aws-tf"
+    }
   }
 }
 
