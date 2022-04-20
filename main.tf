f9cd859a76a9f848554b92aa5d28302838b17c5b
Naoki Oketani <okepy.naoki@gmail.com>
2022-04-20 18:49:55 +0900

    GuardDutyのOraganizations関連設定を削除する

--- main.tf
+++ main.tf
@@ -21,18 +21,11 @@ provider "aws" {
 resource "aws_organizations_organization" "org" {
   aws_service_access_principals = [
     "cloudtrail.amazonaws.com",
-    "guardduty.amazonaws.com",
   ]
   enabled_policy_types = [
     "SERVICE_CONTROL_POLICY"
   ]
   feature_set = "ALL"
-}
-
-resource "aws_guardduty_organization_admin_account" "root" {
-  depends_on = [aws_organizations_organization.org]
-
-  admin_account_id = var.org_admin_id
 }
 
 module "virginia" {
@@ -42,8 +35,6 @@ module "virginia" {
   }
   aws_account_id      = var.org_admin_id
   slack_aws_alert_url = var.slack_aws_alert_url
-
-  depends_on = [aws_guardduty_organization_admin_account.root]
 }
 
 module "tokyo" {
@@ -53,6 +44,4 @@ module "tokyo" {
   }
   aws_account_id      = var.org_admin_id
   slack_aws_alert_url = var.slack_aws_alert_url
-
-  depends_on = [aws_guardduty_organization_admin_account.root]
 }
